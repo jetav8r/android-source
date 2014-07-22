837aeaf7408a18902a92b9f06adbd9fc49599cbc
Wayne Johnson <waynejohnson1965@gmail.com>
2014-07-21 17:39:36 -0700

    Merge branch 'master' of github.com:jetav8r/android-source
    Conflicts:
    	milestone_01_java/00_oop/classes/src/com/bloc/classes/Dog.java

--- milestone_01_java/00_oop/classes/src/com/bloc/classes/Dog.java
+++ milestone_01_java/00_oop/classes/src/com/bloc/classes/Dog.java
@@ -196,7 +196,8 @@ class Dog {
                     System.out.println("Your dog's current size is large");
                     break;    
             }
-            System.out.println(index + newSize + mSize + mMeals);
+            System.out.println("index = " +index + " newSize = " + newSize +
+                    " mSize = " + mSize + " mMeals = " + mMeals);
         } 
 
 	/*
@@ -224,15 +225,19 @@ class Dog {
             }
             switch (newSize) {
                 case 0:
+                    this.mSize = "tiny";
                     System.out.println("Your dog's current size is tiny");
                     break;
                 case 1:
+                    this.mSize = "small";
                     System.out.println("Your dog's current size is small");
                     break;
                 case 2:
+                    this.mSize = "average";
                     System.out.println("Your dog's current size is average");
                     break;
                 case 3:
+                    this.mSize = "large";
                     System.out.println("Your dog's current size is large");
                     break;    
             }
