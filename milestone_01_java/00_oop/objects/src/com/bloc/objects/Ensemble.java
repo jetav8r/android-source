7fadddd06f4d4684f8822ad6525c157bb6936239
Wayne Johnson <waynejohnson1965@gmail.com>
2014-07-31 13:51:32 -0700

    Object passed somehow...feel like I missed something

--- milestone_01_java/00_oop/objects/src/com/bloc/objects/Ensemble.java
+++ milestone_01_java/00_oop/objects/src/com/bloc/objects/Ensemble.java
@@ -15,6 +15,19 @@ class Ensemble extends Object {
 	// CONSTRUCTOR CODE GOES HERE
         Ensemble (Artist[] artists) {
             mArtists= artists;
+            //added code to view variables
+            /*System.out.println("Artist constructor variable line....mName = " + 
+             *       mName +  ", mArtists = " + mArtists[0] + 
+             *       "mFirstName = " + artists[0].getFirstName() + ", mLastName = " +
+             *       artists[0].getLastName() + "name = ?");
+            //Had to add the following line here, because even though it says it wants 
+            //this in the second constructor, Main.java tests it here...grrrrr
+            */
+            if (mName == null) {
+                mName = artists[0].getFirstName() + " " + artists[0].getLastName();
+                //System.out.println("mName null Artist variable line....mName = " + 
+                //    mName);
+            }
         }
      
 	/*
@@ -30,8 +43,15 @@ class Ensemble extends Object {
         Ensemble (String name, Artist[] artists) {
             mName = name;
             mArtists = artists;
-            if (name == null) {
+            //System.out.println("mName = " + mName +  ", mArtists = " + mArtists[0] + 
+            //        "mFirstName = " + artists[0].getFirstName() + ", mLastName = " +
+            //        artists[0].getLastName());
+             
+            if (mName == null) {
                 mName = artists[0].getFirstName() + " " + artists[0].getLastName();
+            //    System.out.println("Null test line----mName = " + mName +  ", mArtists = " + mArtists[0] + 
+            //       "mFirstName = " + artists[0].getFirstName() + ", mLastName = " +
+            //        artists[0].getLastName() + "name = " + name);
             }
         }
 }
