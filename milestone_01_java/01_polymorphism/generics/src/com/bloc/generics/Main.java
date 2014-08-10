0bcb930d586da848de94406fa3ee2ad9d274cd59
Wayne Johnson <waynejohnson1965@gmail.com>
2014-08-10 03:42:42 -0700

    I got this to pass, but I'm still a little lost about types

--- milestone_01_java/01_polymorphism/generics/src/com/bloc/generics/Main.java
+++ milestone_01_java/01_polymorphism/generics/src/com/bloc/generics/Main.java
@@ -10,12 +10,24 @@ public class Main extends Object {
 		/*
 		 * Put a bunch of Toys in toyBox!
 		 */
+                toyBox.addToy(new Toy("Book about confusing generic types"));
+                toyBox.addToy(new Toy("Spoon"));
+                toyBox.addToy(new Toy("Action Figure"));
+                toyBox.addToy(new Toy("Can I Put Anything in here?"));
+                //above added Strings to toy box, but not objects (I think)
+                //what about adding the objects Book, Spoon, ActionFigure?
+                Book book = new Book();
+                toyBox.addToy(new Toy(book));
+                //the above worked, but is it the correct way to do this??
+                
 
 		assert toyBox.getToyCount() > 0 : "Let's get some toys in that box!";
 		System.out.println("Inside your toybox you've got:");
 		for (int i = 0; i < toyBox.getToyCount(); i++) {
-			System.out.println("- " + toyBox.getToyAtIndex(i).get());
+			System.out.println("- " + toyBox.getToyAtIndex(i).get());                        
 		}
+                //added line to print toy count
+                //System.out.println("ToyCount = " + toyBox.getToyCount());
 		System.out.println("Sounds like fun!\n");
 
 		System.out.println("/************************/");
