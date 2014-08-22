6cb6e780030d5c52534b6cd484a8c856b7474872
Wayne Johnson <waynejohnson1965@gmail.com>
2014-08-22 11:59:59 -0700

    not finished

--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
@@ -1,10 +1,21 @@
 package com.bloc.singletons;
 
-public class Main extends Object {
+import com.bloc.singletons.listeners.*;
+import com.bloc.singletons.talkers.*;
+import com.bloc.singletons.Speakerphone;
 
-	public static void main(String [] args) {
+public class Main extends Object {
+    public static void main(String [] args) {
 		// Instantiate some talkers and some listeners
-		// Register listeners
+            Parent mom = new Parent();
+            PetOwner jack = new PetOwner();
+            Singer whitney = new Singer();
+            AudienceMember audience = new AudienceMember();
+            Child boy = new Child();
+            Pet dog = new Pet();
+            	// Register listeners
+            com.bloc.singletons.Speakerphone.addListener(audience);
 		// Send messages!
+            //send talker message....i.e.mom.getMessage()
 	}
 }
--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
@@ -1,5 +1,7 @@
 package com.bloc.singletons;
 
+import java.util.*;
+
 /*
  * This is a singleton class which maintains communication
  * between classes and Listeners
@@ -9,24 +11,47 @@ public class Speakerphone extends Object {
 	 * get
 	 * @return the singleton instance of Speakerphone
 	 */
-
+    private static Speakerphone sSphone;
+    
+    public static Object getInstance() {
+        if (sSphone == null) {
+            sSphone = new Speakerphone();
+        }
+        return sSphone;
+    }
+    
+    private HashSet<Listener> mListener;
+    
+    private Speakerphone() {
+        mListener = new HashSet<>();
+    }
 	/*
 	 * addListener
 	 * Add a listener to Speakerphone's list
 	 * @param listener an instance of the Listener interface
 	 */
-
+    public void addListener(Listener listener) {
+        mListener.add(listener);
+    }
 	/*
 	 * removeListener
 	 * Remove a Listener from the Speakerphone's list
 	 * @param listener the Listener to remove
 	 */
-
+    public void removeListener(Listener listener) {
+        mListener.remove(listener);
+    }
 	/*
 	 * shoutMessage
 	 * Sends the message to all of the Listeners tracked by Speakerphone
 	 * @param talker a Talker whose message will be sent
 	 */
+    public void shoutMessage(Talker talker) {
+       Iterator<Listener> shoutIterator = mListener.iterator();
+       while (shoutIterator.hasNext()) {
+           shoutIterator.next().talker.getMessage();
+       }
+    }
 
 	/*
 	 * shoutMessage
@@ -44,5 +69,8 @@ public class Speakerphone extends Object {
 	 * removeAll
 	 * Removes all Listeners from Speakerphone
 	 */
+    public void removeAll() {
+        this.mListener.clear();
+    }
 
 }
