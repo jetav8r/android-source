73009d43511a892e90211c8546d8be5173d820a0
jetav8r <waynejohnson1965@gmail.com>
2014-09-28 00:48:11 -0700

    database added with notebook and notes tables

--- milestone_02_intro_to_android/BlocNotes/app/src/main/java/com/bloc/blocnotes/BlocNotesHelper.java
+++ milestone_02_intro_to_android/BlocNotes/app/src/main/java/com/bloc/blocnotes/BlocNotesHelper.java
@@ -1,9 +1,14 @@
 package com.bloc.blocnotes;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by Wayne on 9/27/2014.
@@ -13,22 +18,30 @@ public class BlocNotesHelper extends SQLiteOpenHelper {
     private Context context;
     private static final String DATABASE_NAME = "blocnotes";
     private static final String TABLE1_NAME = "notebooks";
-    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE2_NAME = "notes";
+    private static final int DATABASE_VERSION = 3;
     private static final String UID = "_id";
     private static final String nb_id = "nb_identifier";
+    private static final String nt_id = "note_identifier";
     private static final String name = "name ";
+    private static final String body = "body";
     private static final String description = "description";
-    private static final String DROP_TABLE= "DROP TABLE IF EXISTS" +TABLE1_NAME+"";
+    private static final String reference = "notebook_reference";
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE1_NAME + "";
 
     private static final String CREATE_NOTEBOOKS =
-            "CREATE TABLE "+TABLE1_NAME+
-                    "("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT," +nb_id+" VARCHAR(255),"
-                    +name+" VARCHAR(255)," +description+" VARCHAR(255)" +" )";
+            "CREATE TABLE " + TABLE1_NAME +
+                    "(" + UID + " INTEGER PRIMARY KEY AUTOINCREMENT," + nb_id + " VARCHAR(255),"
+                    + name + " VARCHAR(255)," + description + " VARCHAR(255)" + " )";
+    private static final String CREATE_NOTES =
+            "CREATE TABLE " + TABLE2_NAME +
+                    "(" + UID + " INTEGER PRIMARY KEY AUTOINCREMENT," + nt_id + " VARCHAR(255),"
+                    + body + " VARCHAR(255)," + reference + " VARCHAR(255)" + " )";
 
     public BlocNotesHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context=context;
-        Message.message(context,"constructor was called");
+        this.context = context;
+        Message.message(context, "constructor was called");
     }
 
     @Override
@@ -36,9 +49,10 @@ public class BlocNotesHelper extends SQLiteOpenHelper {
 
         try {
             sqLiteDatabase.execSQL(CREATE_NOTEBOOKS);
-            Message.message(context,"onCreate was called");
+            sqLiteDatabase.execSQL(CREATE_NOTES);
+            Message.message(context, "onCreate was called");
         } catch (SQLException e) {
-            Message.message(context,""+e);
+            Message.message(context, "" + e);
         }
 
     }
@@ -47,11 +61,10 @@ public class BlocNotesHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
         try {
             sqLiteDatabase.execSQL(DROP_TABLE);
-            Message.message(context,"onUpgrade was called");
+            Message.message(context, "onUpgrade was called");
             onCreate(sqLiteDatabase);
         } catch (SQLException e) {
-            Message.message(context,""+e);
+            Message.message(context, "" + e);
         }
-
     }
 }
