f9669ed820e25b337fff3446bf6cae403769f392
jetav8r <waynejohnson1965@gmail.com>
2014-12-23 10:44:32 -0800

    initial commit

---
+++ milestone_02_intro_to_android/Blocly/app/src/main/java/com/bloc/android/ui/activity/BloclyActivity.java
@@ -0,0 +1,18 @@
+package com.bloc.android.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.bloc.blocly.R;
+
+/**
+ * Created by Wayne on 12/23/2014.
+ */
+public class BloclyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+    }
+}
