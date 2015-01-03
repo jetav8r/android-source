48dd2c99582539bb8e351a7ff272181d6c330c81
jetav8r <waynejohnson1965@gmail.com>
2015-01-03 15:49:19 -0800

    checkpoint-45-completed

---
+++ milestone_02_intro_to_android/Blocly/app/src/main/java/com/bloc/android/blocly/ui/adapter/NavigationDrawerAdapter.java
@@ -0,0 +1,141 @@
+package com.bloc.android.blocly.ui.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bloc.android.blocly.BloclyApplication;
+import com.bloc.android.blocly.api.model.RssFeed;
+import com.bloc.blocly.R;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by Wayne on 1/2/2015.
+ */
+public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDrawerAdapter.ViewHolder> {
+    // track these constants and their positions in the list with an enumeration
+    public enum NavigationOption {
+        NAVIGATION_OPTION_INBOX,
+        NAVIGATION_OPTION_FAVORITES,
+        NAVIGATION_OPTION_ARCHIVED
+    }
+
+    public static interface NavigationDrawerAdapterDelegate {
+        public void didSelectNavigationOption(NavigationDrawerAdapter adapter, NavigationOption navigationOption);
+        public void didSelectFeed(NavigationDrawerAdapter adapter, RssFeed rssFeed);
+    }
+
+    // WeakReference object to store our delegate. A WeakReference allows us to use an object
+    // as long as a strong reference to it exists somewhere
+    WeakReference<NavigationDrawerAdapterDelegate> delegate;
+
+    //getter and setter for delegate
+    //Use WeakReference.get() to recover the object within, but remember, if the original reference
+    // has been removed, this method will return null.
+    public NavigationDrawerAdapterDelegate getDelegate() {
+        if (delegate == null) {
+            return null;
+        }
+        return delegate.get();
+    }
+
+    public void setDelegate(NavigationDrawerAdapterDelegate delegate) {
+        this.delegate = new WeakReference<NavigationDrawerAdapterDelegate>(delegate);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int position) {
+        View inflate = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.navigation_item, viewGroup, false);
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        RssFeed rssFeed = null;
+        // bind each ViewHolder to a position
+        //We use the total number of navigational elements, NavigationOption.values().length, to offset our position
+        // into the full RssFeed array. This is done because the first RssFeed item is located at position 3 within
+        // the adapter but at position 0 in the original RssFeed array.
+        if (position >= NavigationOption.values().length) {
+            int feedPosition = position - NavigationOption.values().length;
+            rssFeed = BloclyApplication.getSharedDataSource().getFeeds().get(feedPosition);
+        }
+        viewHolder.update(position, rssFeed);
+    }
+
+    @Override
+    public int getItemCount() {
+    // number of items our Adapter provides
+        return NavigationOption.values().length
+                + BloclyApplication.getSharedDataSource().getFeeds().size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        View topPadding;
+        TextView title;
+        View bottomPadding;
+        View divider;
+
+        int position;
+        RssFeed rssFeed;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            topPadding = itemView.findViewById(R.id.v_nav_item_top_padding);
+            title = (TextView) itemView.findViewById(R.id.tv_nav_item_title);
+            bottomPadding = itemView.findViewById(R.id.v_nav_item_bottom_padding);
+            divider = itemView.findViewById(R.id.v_nav_item_divider);
+            itemView.setOnClickListener(this);
+        }
+
+        void update(int position, RssFeed rssFeed) {
+            this.position = position;
+            this.rssFeed = rssFeed;
+            boolean shouldShowTopPadding = position == NavigationOption.NAVIGATION_OPTION_INBOX.ordinal()
+                    || position == NavigationOption.values().length;
+            topPadding.setVisibility(shouldShowTopPadding ? View.VISIBLE : View.GONE);
+
+            boolean shouldShowBottomPadding = position == NavigationOption.NAVIGATION_OPTION_ARCHIVED.ordinal()
+                    || position == getItemCount() - 1;
+            bottomPadding.setVisibility(shouldShowBottomPadding ? View.VISIBLE : View.GONE);
+
+            divider.setVisibility(position == NavigationOption.NAVIGATION_OPTION_ARCHIVED.ordinal()
+                    ? View.VISIBLE : View.GONE);
+
+            if (position < NavigationOption.values().length) {
+            // create a local array with our three primary titles arranged in order within it
+                int[] titleTexts = new int[] {R.string.navigation_option_inbox,
+                        R.string.navigation_option_favorites,
+                        R.string.navigation_option_archived};
+                title.setText(titleTexts[position]);
+            } else {
+                title.setText(rssFeed.getTitle());
+            }
+        }
+
+
+         /*
+          * OnClickListener
+          */
+
+        @Override
+        public void onClick(View view) {
+            //Message.message(view.getContext(), "Nothing… yet!");
+            if (getDelegate() == null) {
+                return;
+            }
+            if (position < NavigationOption.values().length) {
+            // provide a reference to the NavigationDrawerAdapter
+                getDelegate().didSelectNavigationOption(NavigationDrawerAdapter.this,
+                        NavigationOption.values()[position]);
+            } else {
+            // provide a reference to the NavigationDrawerAdapter
+                getDelegate().didSelectFeed(NavigationDrawerAdapter.this, rssFeed);
+            }
+        }
+    }
+}
